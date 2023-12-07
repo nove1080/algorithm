168fcedbf59ede393e4936426c70948f72b7ba45
nove1080 <skwpqjq@gmail.com>
2023-12-07 23:35:51 +0900

    add: 이전 풀이 업로드

---
+++ src/Dynamic_Programming/Boj11659.java
@@ -0,0 +1,49 @@
+//구간 합 구하기 4
+package Dynamic_Programming;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj11659 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m;
+    static int[] sums; //idx까지 더한 합
+
+    public static void main(String[] args) throws Exception {
+        init();
+        while(m-- != 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            save(dp(i, j));
+        }
+        System.out.println(sb);
+    }
+
+    static void init() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        sums = new int[n];
+        sums[0] = Integer.parseInt(st.nextToken());
+        for(int i = 1; i < n; i++) {
+            sums[i] = sums[i-1] + Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static int dp(int i, int j) {
+        if(i > 1)
+            return sums[j - 1] - sums[i - 2];
+        else
+            return sums[j - 1];
+    }
+
+    static void save(int sum) {
+        sb.append(sum).append("\n");
+    }
+}
---
+++ src/Dynamic_Programming/Boj2579.java
@@ -0,0 +1,77 @@
+//계단 오르기
+
+/*
+    1. 테이블 정의하기
+        D[i][j] = i번째 계단에서 j만큼 연속해서 계단을 올랐을 때, 점수 합의 최대값
+    2. 점화식 찾기
+        D[k][1] = max(D[k-2][2], D[k-2][1]) + score[k]
+        D[k][2] = D[k-1][1] + score[k]
+    3. 초기값 정하기
+        D[1][1]
+ */
+
+/*
+    풀이 2
+       1. 테이블 정의하기
+        D[1] = score[1], D[2] = score[2],
+        D[3] = D[1] + score[3],
+        D[4] =
+
+       2. 점화식 찾기
+        * k번째 안 밟는다 -> k-1번째는 반드시 밟는다., k-2는 반드시 안 밟는다? *
+        D[k] = D[k-2] + score[k]
+
+        if (k == n) then
+
+       3. 테이블 채우기
+         D[1] = 10, D[2] = 20, D[3] = 15,
+         D[4] = 25 + min(s[k-2], s[k-3]) (35);
+         D[5] = 10 + min(s[k-2], s[k-3]) (25);
+
+*/
+package Dynamic_Programming;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj2579 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static int n;
+    static int[] stairs;
+    static int[][] scores; //각 계단(idx)에서의 최대 점수
+
+    public static void main(String[] args) throws Exception {
+        init();
+        System.out.println(dp());
+    }
+
+    static void init() throws Exception {
+        n = Integer.parseInt(br.readLine());
+        stairs = new int[n+1];
+        scores = new int[n+1][3];
+
+        for (int i = 1; i <= n; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        if(n >= 1) {
+            scores[1][1] = stairs[1];
+            scores[1][2] = 0;
+        }
+
+        if(n >= 2) {
+            scores[2][1] = stairs[2];
+            scores[2][2] = stairs[1] + stairs[2];
+        }
+    }
+
+    static int dp() {
+        for(int i = 3; i <= n; i++) {
+            scores[i][1] = Math.max(scores[i - 2][2], scores[i - 2][1]) + stairs[i];
+            scores[i][2] = scores[i - 1][1] + stairs[i];
+        }
+        return Math.max(scores[n][1], scores[n][2]);
+    }
+}
