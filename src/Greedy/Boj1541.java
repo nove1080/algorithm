71a2232153c43151f406522a618375b3f0925f84
nove1080 <skwpqjq@gmail.com>
2024-02-06 21:13:52 +0900

    feat: 24.02.06

--- src/Greedy/Boj1541.java
+++ src/Greedy/Boj1541.java
@@ -1,11 +1,8 @@
 //잃어버린 괄호
 /**
- * 55 - (50 + 40) = -35
- * [풀이]
- * - 가 등장하면 괄호를 사용하여 가장 크게 만들어준다
- *
- * + -> 앞 뒤의 숫자를 더해준다
- * - -> 이제부터 minus 영역에서 연산할 것을 표시해둔다.
+ * 1. '-' 부분을 기준으로 문자열을 파싱한다.
+ * 2. 파싱된 문자열 내부를 합친다.
+ * 3. num1 - num2 - ... - numN 계산한다.
  */
 package Greedy;
 
@@ -14,45 +11,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj1541 {
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws Exception {
-        solve();
-    }
 
-    static void solve() throws Exception {
+        int result = 0;
+        boolean isFirst = true;
+
         StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+        while (st.hasMoreTokens()) {
+            int sum = 0;
 
-        int sum = 0;
-        int buffer = 0;
-        char[] positive = st.nextToken().toCharArray();
-        for (int i = 0; i < positive.length; i++) {
-            if(positive[i] == '+') {
-                sum += buffer;
-                buffer = 0;
-                continue;
+            StringTokenizer addition = new StringTokenizer(st.nextToken(), "+");
+            while (addition.hasMoreTokens()) {
+                sum += Integer.parseInt(addition.nextToken());
             }
-            buffer *= 10;
-            buffer += positive[i] - '0';
-        }
-        sum += buffer;
-        buffer = 0;
 
-        while(st.hasMoreTokens()){
-            char[] negative = st.nextToken().toCharArray();
-            for (int i = 0; i < negative.length; i++) {
-                if(negative[i] == '+') {
-                    sum -= buffer;
-                    buffer = 0;
-                    continue;
-                }
-                buffer *= 10;
-                buffer += negative[i] - '0';
+            if(isFirst) {
+                result += sum;
+                isFirst = false;
+            } else {
+                result -= sum;
             }
-            sum -= buffer;
-            buffer = 0;
         }
-        System.out.println(sum);
+
+        System.out.println(result);
     }
+
 
 }
