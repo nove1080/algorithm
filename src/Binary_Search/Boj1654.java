b9f0a0f984c913b3480763537dc8d1e61e50ad36
nove1080 <skwpqjq@gmail.com>
2024-02-21 21:44:23 +0900

    feat: 24.02.21

--- src/Binary_Search/Boj1654.java
+++ src/Binary_Search/Boj1654.java
@@ -1,19 +1,11 @@
 //1654: 랜선 자르기
-/*
-1. 랜선을 모두 합한다.
-2. 최대 길이 = 합한 랜선 길이 / 필요한 랜선 수
-3. 각 랜선을 [설정한 길이]로 나눈 몫을 합한 값이 reqCables와 같은지 확인
-4. 적다 -> 설정한 길이--
-5. 많다 -> 설정한 길이++
-6. 더 늘리면 reqCables를 충족하지 못할 때까지 (4 or 5) -> 3을 반복
- */
-package Test;
+package Binary_Search;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj1654 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int ownCables, reqCables;
     static int[] cables;
@@ -31,7 +23,7 @@ public class Main {
 
         cables = new int[ownCables];
 
-        int sum = 0;
+        long sum = 0;
         for (int i = 0; i < ownCables; i++) {
             cables[i] = Integer.parseInt(br.readLine());
             sum += cables[i];
