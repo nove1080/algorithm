aa9c14b2c2d54a0dd415e88ca3a21399489f747e
nove1080 <skwpqjq@gmail.com>
2024-03-12 14:24:29 +0900

    feat: 24.03.12

---
+++ src/Hash/Boj7785.java
@@ -0,0 +1,50 @@
+package Hash;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Boj7785 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static HashMap<String, String> employees = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        solve();
+        printAnswer();
+    }
+
+    static void solve() throws Exception {
+        n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String status = st.nextToken();
+            employees.put(name, status);
+        }
+    }
+    
+    static void printAnswer() {
+        StringBuilder sb = new StringBuilder();
+        List<String> names = new ArrayList<>();
+        for(String name : employees.keySet()) {
+            if(employees.get(name).equals("enter")) {
+                names.add(name);
+            }
+        }
+
+        names.sort(Collections.reverseOrder());
+
+        for(String name : names) {
+            sb.append(name)
+              .append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
