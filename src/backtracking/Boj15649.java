143da28a3834574f73a783addb41047fd951d0c8
나제법 <skwpqjq@gmail.com>
2024-11-15 23:45:52 +0900

    feat: 24.11.15

--- src/backtracking/Boj15649.java
+++ src/backtracking/Boj15649.java
@@ -1,54 +1,44 @@
 // N과 M(1) [S3]
 package backtracking;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Boj15649 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
 	static int n, m;
-	static int arr[];
-	static boolean isUsed[];
-	
-	
-	static void init() {
-		arr = new int[m];
-		isUsed = new boolean[n];
-	}
-	
-	static void solve(int x) {
-		if(x == m) {
-			save(arr);
-			return;
-		}
-		
-		for(int i = 0; i < n; i++) {
-			if(isUsed[i] == true) continue;
-			
-			arr[x] = i + 1;
-			isUsed[i] = true;
-			solve(x+1);
-			isUsed[i] = false;
-		}
-	}
-	
-	static void save(int[] arr) {
-		for(int k : arr) {
-			sb.append(k + " ");
-		}
-		sb.append("\n");
-	}
-	
+	static boolean[] vis;
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		init();
-		solve(0);
+	    init();
+		backtracking(0, new int[m]);
 		System.out.println(sb);
 	}
 
+	public static void init() throws Exception {
+		String[] input = br.readLine().split(" ");
+		n = Integer.parseInt(input[0]);
+		m = Integer.parseInt(input[1]);
+		vis = new boolean[n];
+	}
+
+	public static void backtracking(int depth, int[] arr) {
+		if (depth == m) {
+			for (int i = 0; i < arr.length; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (vis[i]) continue;
+			vis[i] = true;
+			arr[depth] = i + 1;
+			backtracking(depth + 1, arr);
+			vis[i] = false;
+		}
+	}
 }
