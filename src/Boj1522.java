56a843f4923d567efab39e902bd42f20f36e5cc1
nove1080 <skwpqjq@gmail.com>
2025-01-21 17:35:13 +0900

    feat: 25.01.21

---
+++ src/Boj1522.java
@@ -0,0 +1,19 @@
+//문자열 교환
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj1522 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static String str;
+
+    public static void main(String[] args) throws Exception {
+        init();
+    }
+
+    public static void init() throws Exception {
+        str = br.readLine();
+    }
+
+}
---
+++ src/Boj17609.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj17609 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int tc;
+
+    public static void main(String[] args) throws Exception {
+        init();
+        System.out.println(sb);
+    }
+
+    public static void init() throws Exception {
+        tc = Integer.parseInt(br.readLine());
+        for (int i = 0; i < tc; i++) {
+            char[] cArr = br.readLine().toCharArray();
+            isPalindrome(cArr, 0, cArr.length - 1, 0);
+        }
+    }
+
+    public static void isPalindrome(char[] cArr, int left, int right, int mismatchCount) {
+        if (mismatchCount > 1) {
+            sb.append(mismatchCount).append("\n");
+            return;
+        }
+
+        while (left <= right) {
+            if (cArr[left] == cArr[right]) {
+                left++;
+                right--;
+                continue;
+            }
+
+            //한쪽씩 지우고 비교해보기
+            int tmpLeft = left + 1;
+            if (cArr[tmpLeft] == cArr[right] && mismatchCount == 0) {
+                left = tmpLeft;
+                left++;
+                right--;
+                isPalindrome(cArr, left, right, mismatchCount + 1);
+            }
+
+            int tmpRight = right - 1;
+            if (cArr[left] == cArr[tmpRight] && mismatchCount == 0) {
+                left++;
+                right = tmpRight;
+                right--;
+                isPalindrome(cArr, left, right, mismatchCount + 1);
+            }
+        }
+
+        return;
+    }
+
+}
