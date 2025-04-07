a6288553cdb078037d87b6e23a98ed6b8e82ac06
nove1080 <skwpqjq@gmail.com>
2025-04-07 15:25:00 +0900

    feat: Boj1927.java

---
+++ src/priorityqueue/Boj1927.java
@@ -0,0 +1,35 @@
+package priorityqueue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Boj1927 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    static int n;
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            if (input == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            } else {
+                pq.add(input);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void init() throws Exception {
+        n = Integer.parseInt(br.readLine());
+    }
+
+}
