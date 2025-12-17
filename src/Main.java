f52084de0f67f5e8253b15ac5fc86421f425c31a
나제법 <skwpqjq@gmail.com>
2025-12-17 11:20:05 +0900

    feat: Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        init();
+    }
+
+    public static void init() throws Exception {
+
+    }
 }
 
